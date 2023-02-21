cad478f45b7df3dc426015255c7bf1e78736a6d3
C. Boyardee <coyoteandbird@gmail.com>
2023-02-20 22:44:24 -0500

    Code cleanup

--- README.md
+++ README.md
@@ -2,13 +2,27 @@
 
 It is a leaderboard for displaying slippi rankings
 
+#### Install & Enter VirtualEnv
+
+`poetry install`
+
+`poetry shell`
+
 #### Django commands
 
 `python manage.py runserver_plus`
 
 Run development server
 
+`export SECRET_KEY=$(python manage.py generate_password --length 50)`
+
+Generate a secret key
+
 `python manage.py makemigrations`
+
+Run with hypercorn:
+
+`hypercorn slippi_leaderboard.asgi:application`
 
 Make some changes to the database? Generate the necessary migrations
 
--- settings/base.py
+++ settings/base.py
@@ -12,15 +12,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s2=_ttw&=rl8$ss*yc&2y!h6ge9hqkx=(vd0in*7#008_)@5kv'
+SECRET_KEY = 'thisisnotaverysecurekeyatall'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,12 +72,7 @@ WSGI_APPLICATION = 'slippi_leaderboard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
